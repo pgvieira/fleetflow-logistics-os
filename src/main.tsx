ce66ca4e29fc20807c195b5e58fc32fc5da615c6
Paulo Gustavo <gustavo.pgvi@gmail.com>
2026-09-23 08:51:30 -0300

    chore: add global styles and theme variables, include CSS reset, update version to 0.0.8

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot, type Root } from 'react-dom/client';
+import './styles/index.css';
 import AppComponent from './App.tsx';
 
 const rootElement: HTMLElement = document.getElementById('root')!;
